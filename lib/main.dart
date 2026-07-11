645ccdd7df9e8edd85797d686af33d0463118230
AbdallahMahmoud88 <Abdullah.0123.com@gmail.com>
2026-07-11 18:35:31 +0300

    Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -12,8 +13,7 @@ class NotesApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: Scaffold(),
-      
+      home: const NotesView(),
     );
   }
 }
